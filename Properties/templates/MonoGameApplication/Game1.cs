559acdafd1da6a6b188a58c141d353d2cc0ea060
stephen byrne <gold.mine.labs@gmail.com>
2014-06-05 12:00:15 +0100

    upload to git

---
+++ Properties/templates/MonoGameApplication/Game1.cs
@@ -0,0 +1,90 @@
+﻿#region Using Statements
+using System;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.Input;
+
+#endregion
+
+namespace ${Namespace}
+{
+	/// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+		Texture2D logoTexture;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+			Content.RootDirectory = "Assets";	            
+			graphics.IsFullScreen = false;		
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            base.Initialize();
+				
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            //TODO: use this.Content to load your game content here 
+			logoTexture = Content.Load<Texture2D>("logo");
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // For Mobile devices, this logic will close the Game when the Back button is pressed
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+			{
+				Exit();
+			}
+            // TODO: Add your update logic here			
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+           	graphics.GraphicsDevice.Clear(Color.CornflowerBlue);
+		
+            //TODO: Add your drawing code here
+			spriteBatch.Begin();
+
+			// draw the logo
+			spriteBatch.Draw(logoTexture, new Vector2 (130, 200), Color.White);
+
+			spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Properties/templates/MonoGameApplication/OUYAActivity1.cs
@@ -0,0 +1,38 @@
+using System;
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Android.Content;
+using Ouya.Console.Api;
+
+using Microsoft.Xna.Framework;
+
+namespace ${Namespace}
+{
+	[Activity (Label = "${ProjectName}", 
+	           MainLauncher = true,
+	           Icon = "@drawable/icon",
+	           Theme = "@style/Theme.Splash",
+                   AlwaysRetainTaskState=true,
+	           LaunchMode=LaunchMode.SingleInstance,
+	           ConfigurationChanges = ConfigChanges.Orientation | 
+	                                  ConfigChanges.KeyboardHidden | 
+	                                  ConfigChanges.Keyboard)]
+        [IntentFilter(new[] { Intent.ActionMain }
+            , Categories = new[] { Intent.CategoryLauncher, OuyaIntent.CategoryGame })]
+	public class Activity1 : AndroidGameActivity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Create our OpenGL view, and display it
+			Game1.Activity = this;
+			var g = new Game1();
+			SetContentView (g.Window);
+			g.Run();
+		}
+		
+	}
+}
+
